311d0f99d7ad788103254b2bfa9b5a96fb183c1c
Lourenzo Acunzo <lourenzo_acunzo@hotmail.com>
2023-06-20 17:10:50 -0300

    Aba Contas

--- Fintech.Dominio/Entidades/ContaCorrente.cs
+++ Fintech.Dominio/Entidades/ContaCorrente.cs
@@ -8,6 +8,13 @@ namespace Fintech.Dominio.Entidades
 {
     public class ContaCorrente : Conta
     {
+        public ContaCorrente(Agencia agencia, int numero, string digitoVerificador)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            DigitoVerificador = digitoVerificador;
+        }
+
         public bool EmissaoChequeHabilitada { get; set; }
     }
 }
