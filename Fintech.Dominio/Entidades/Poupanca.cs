59b5635e10c71542a1593711316e326d3584a439
Lourenzo Acunzo <lourenzo_acunzo@hotmail.com>
2023-06-21 16:15:57 -0300

    Aba de contas

--- Fintech.Dominio/Entidades/Poupanca.cs
+++ Fintech.Dominio/Entidades/Poupanca.cs
@@ -8,6 +8,13 @@ namespace Fintech.Dominio.Entidades
 {
     public class Poupanca : Conta
     {
+        public Poupanca(Agencia agencia, int numero, string digitoVerificador)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            DigitoVerificador = digitoVerificador;
+        }
+
         public decimal TaxaRendimento { get; set; }
     }
 }
