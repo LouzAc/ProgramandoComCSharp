ed16c15740052a9208a09dad387e3f7dd7f10bde
Lourenzo Acunzo <lourenzo_acunzo@hotmail.com>
2023-06-26 22:33:24 -0300

    Inserção de Movimento

--- Fintech.Dominio/Entidades/ContaEspecial.cs
+++ Fintech.Dominio/Entidades/ContaEspecial.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Fintech.Dominio.Entidades
+﻿namespace Fintech.Dominio.Entidades
 {
     public class ContaEspecial : ContaCorrente
     {
@@ -14,28 +8,10 @@ namespace Fintech.Dominio.Entidades
         }
 
         public decimal Limite { get; set; }
-        public override void EfetuarOperacao(decimal valor, Operacao operacao)
+
+        public override Movimento EfetuarOperacao(decimal valor, Operacao operacao, decimal limite = 0)
         {
-            var sucesso = true;
-
-            switch (operacao)
-            {
-                case Operacao.Deposito:
-                    Saldo += valor;
-                    break;
-                case Operacao.Saque:
-                    if (Saldo + Limite >= valor)
-                    {
-                        Saldo -= valor;
-                    }
-                    else
-                    {
-                        sucesso = false;
-                    }
-                    break;
-            }
-
-            if (sucesso) Movimentos.Add(new Movimento(operacao, valor, this));
+            return base.EfetuarOperacao(valor, operacao, Limite);
         }
     }
 }
